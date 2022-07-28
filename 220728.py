db7f1aeb4f256436d8e45d203c1b7dfc9554e37f
ehdrjs5161 <ehdjrs5161@gmail.com>
2022-07-28 14:54:24 +0900

    update

--- 220728.py
+++ 220728.py
@@ -1,13 +1,13 @@
 from sklearn.impute import SimpleImputer as SI
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, accuracy_score
 import pandas as pd
 
 def drop_col(df):
     col_with_missing_val = [col for col in df.columns if df[col].isnull().any()]
-    print(col_with_missing_val)
     df.drop(col_with_missing_val, axis=1) 
     return df
 
@@ -25,14 +25,6 @@ def label_with_OE(df):
 
     return df
 
-def group_age(df):
-    age_group = []
-    for i in range(len(df)):
-        age_group.append(int(df['Age'].iloc[i]/10))
-
-    df['Age'] = age_group
-    return df
-
 def find_best_model(x_data, y_data):
     X_train, X_valid, y_train, y_valid = train_test_split(x_data, y_data,
                                                       train_size=0.8, test_size=0.2,
@@ -44,19 +36,29 @@ def find_best_model(x_data, y_data):
         predicts = model.predict(X_valid)
         score.append((i, mean_absolute_error(y_valid, predicts)))
 
+def logistic_regression(train_x, train_y, test_x, test_y):
+    logreg = LogisticRegression()
+    logreg.fit(train_x, train_y)
+    y_pred = logreg.predict(test_x)
+    acc = round(logreg.score(train_x, train_y)*100, 2)
+    test_acc = accuracy_score(test_y, y_pred)
+    print("----------Logistic Regression Accuracy----------")
+    print("acc: ", acc)
+    print("test acc: ", test_acc)
+
+    return y_pred
+
 if __name__ == "__main__":
     X_train = pd.read_csv("titanic/train.csv")
     X_train.drop(["PassengerId", 'Name', "Cabin", 'Ticket'], axis=1, inplace=True)
     X_train.dropna(subset=['Embarked'], axis=0, inplace=True)
 
-    categorical_col = [col for col in X_train.columns if X_train[col].dtype == 'object']
-    print(categorical_col)
+    # categorical_col = [col for col in X_train.columns if X_train[col].dtype == 'object']
+    # print(categorical_col)
 
     label_OE_df = label_with_OE(X_train)
-    imputed_X_train = impute_col(label_OE_df)
-    imputed_X_train['Age'] = imputed_X_train['Age'].astype(int)
-
-    final_X_train = group_age(imputed_X_train)    
+    final_X_train = impute_col(label_OE_df)
+  
     final_Y_train = final_X_train.pop("Survived")
 
     # find_best_model(final_X_train, final_Y_train)
@@ -67,13 +69,18 @@ if __name__ == "__main__":
     test_data.drop(['Name', "Cabin", 'Ticket'], axis=1, inplace=True)
 
     label_test_data = label_with_OE(test_data)
-    imputed_test_data = impute_col(test_data)
-    final_X_test = group_age(imputed_test_data)
+    final_X_test = impute_col(test_data)
     final_Y_test = final_X_test.pop("Survived")
 
     best_model = RandomForestClassifier(n_estimators=100, random_state=0)
     best_model.fit(final_X_train, final_Y_train)
     predicts = best_model.predict(final_X_test)
 
-    score = accuracy_score(final_Y_test, predicts)
-    print(score)
+    Y_test['Survived'] = predicts
+    Y_test['Survived'] = Y_test['Survived'].astype(int)
+    print(Y_test['Survived'].astype)
+    Y_test.to_csv("hf5161_rfmodel_titanic.csv", index=False)
+
+    # score = accuracy_score(final_Y_test, predicts)
+    # print(score)
+    # answer = logistic_regression(final_X_train, final_Y_train, final_X_test, final_Y_test)
